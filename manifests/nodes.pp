13194c5a5b073a4eeb9e90b7bce4648951bd6e52
Ionut Nicolae <nicolae_ionut91@yahoo.com>
2014-12-23 15:42:24 +0000

    sistem message

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,6 +1,9 @@
 node 'ionut.chml.ro' {
     include puppet
     include apache
+    if ($::operatingsystem == 'CentOS') and (($::architecture == 'x86_64') or ($::architecture == 'i386')) {
+        notify { "Sistemul de operare este CentOS $::architecture": }
+    }
 }
 
 node 'puppet.srv.ro'{
